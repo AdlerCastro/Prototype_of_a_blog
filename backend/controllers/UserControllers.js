83a5131ffe06b9391072ece0fb37893b0e3ad600
AdlerCastro <adlercastro01@gmail.com>
2024-07-09 13:36:31 -0300

    :sparkles: feat: Criando função de Login e CheckUser

--- backend/controllers/UserControllers.js
+++ backend/controllers/UserControllers.js
@@ -1,5 +1,12 @@
 const User = require('../models/User')
 
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+// Helpers
+const createUserToken = require('../helpers/create-user-token')
+const getToken = require('../helpers/get-token')
+
 module.exports = class UserController{
     static async register(req, res) {
         const { name, email, phone, password, confirmPassword} = req.body
@@ -38,5 +45,76 @@ module.exports = class UserController{
             res.status(422).json({message: 'Esse email já está sendo utilizado'})
             return
         }
+
+        // Creating the encrypted password
+        const salt = await bcrypt.genSalt(12) // Encryption parameters
+        const passwordHash = await bcrypt.hash(password, salt) // Encrypted password
+
+        // Create a user
+        const user = new User({
+            name: name,
+            email: email,
+            phone: phone,
+            password: passwordHash,
+        })
+
+        try {
+            const newUser = await user.save()
+            
+            await createUserToken(newUser, req, res)
+
+        } catch (error) {
+            res.status(500).json({message: error})
+        }
+    }
+
+    static async login(req, res){
+
+        const {email, password} = req.body
+
+        if(!email) {
+            res.status(422).json({ message: 'Email obrigatório'})
+            return
+        }
+
+        if(!password) {
+            res.status(422).json({ message: 'Senha obrigatório'})
+            return
+        }
+
+        // Check if user exists
+        const user = await User.findOne({ email: email})
+
+        if(!user){
+            res.status(422).json({message: 'Email não cadastrado'})
+            return
+        }
+
+        // Check if password match with db password
+        const checkPassword = await bcrypt.compare(password, user.password)
+
+        if(!checkPassword) {
+            res.status(422).json({message: 'Senha inválida'})
+            return
+        }
+
+        await createUserToken(user, req, res)
+    }
+
+    static async checkUser(req, res){
+        let currentUser
+
+        if(req.headers.authorization){
+            const token = getToken(req)
+            const decoded = jwt.verify(token, 'secret')
+
+            currentUser = await User.findById(decoded.id)
+
+            currentUser.password = undefined
+        } else{
+            currentUser = null
+        }
+
+        res.status(200).send(currentUser)
     }
 }
