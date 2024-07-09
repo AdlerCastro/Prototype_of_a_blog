dc932351efe18135ce0df2c3c7345ba74c2e6c40
AdlerCastro <adlercastro01@gmail.com>
2024-07-09 17:43:20 -0300

    :construction: Desenvolvendo as rotas

---
+++ blog/src/pages/api/controllers/UserControllers.ts
@@ -0,0 +1,7 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async (req:NextApiRequest, res:NextApiResponse) => {
+
+}
