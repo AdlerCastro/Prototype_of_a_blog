5bced85a269673d66c76b403c62d905871b5ff0f
AdlerCastro <adlercastro01@gmail.com>
2024-07-10 20:35:19 -0300

    :fire: Reiniciando o projeto

--- backend/routes/UserRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-
-const UserController = require('../controllers/UserControllers')
-
-router.post('/register', UserController.register)
-router.post('/login', UserController.login)
-router.get('/checkuser', UserController.checkUser)
-
-module.exports = router
--- blog/src/pages/api/controllers/UserControllers.ts
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt'
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async (req:NextApiRequest, res:NextApiResponse) => {
-
-}
