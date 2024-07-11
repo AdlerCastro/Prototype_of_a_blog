b8157fcf4f110a6d4670eca57e561880a6bdf30b
AdlerCastro <adlercastro01@gmail.com>
2024-07-11 13:14:06 -0300

    :truck: Movendo para components em src

---
+++ blog/src/components/organisms/Footer/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='contacts'>
+        <h2>Contacts</h2>
+        <p>b1-9u1u24-94i=210</p>
+      </div>
+      <div>
+        <p>Desenvolvido por Adler Castro</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
