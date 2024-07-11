c61b065b05689025db7bb7e382adf272773587bd
AdlerCastro <adlercastro01@gmail.com>
2024-07-11 16:42:46 -0300

    :white_check_mark: test: Comentado temporariamente

--- blog/src/app/api/auth/[...nextauth]/route.ts
+++ blog/src/app/api/auth/[...nextauth]/route.ts
@@ -1,38 +1,38 @@
-import NextAuth from 'next-auth'
-import CredentialsProvider from "next-auth/providers/credentials"
+// import NextAuth from 'next-auth'
+// import CredentialsProvider from "next-auth/providers/credentials"
 
-const handler = NextAuth({
+// const handler = NextAuth({
 
-    pages:{
-        signIn: '/Login'
-    },
+//     pages:{
+//         signIn: '/Login'
+//     },
 
-    providers:[
-        CredentialsProvider({  
-          name: 'Credentials',
+//     providers:[
+//         CredentialsProvider({  
+//           name: 'Credentials',
 
-          credentials: {
-            username: { label: "Username", type: "text", placeholder: "jsmith" },
-            password: { label: "Password", type: "password" }
-          },
-          async authorize(credentials, req) {
+//           credentials: {
+//             username: { label: "Username", type: "text", placeholder: "jsmith" },
+//             password: { label: "Password", type: "password" }
+//           },
+//           async authorize(credentials, req) {
 
-            const res = await fetch("/your/endpoint", {
-              method: 'POST',
-              body: JSON.stringify(credentials),
-              headers: { "Content-Type": "application/json" }
-            })
-            const user = await res.json()
+//             const res = await fetch("/your/endpoint", {
+//               method: 'POST',
+//               body: JSON.stringify(credentials),
+//               headers: { "Content-Type": "application/json" }
+//             })
+//             const user = await res.json()
       
-            // If no error and we have user data, return it
-            if (res.ok && user) {
-              return user
-            }
-            // Return null if user data could not be retrieved
-            return null
-          }
-        })
-      ]
-})
+//             // If no error and we have user data, return it
+//             if (res.ok && user) {
+//               return user
+//             }
+//             // Return null if user data could not be retrieved
+//             return null
+//           }
+//         })
+//       ]
+// })
 
-export {handler as GET, handler as POST}
+// export {handler as GET, handler as POST}
