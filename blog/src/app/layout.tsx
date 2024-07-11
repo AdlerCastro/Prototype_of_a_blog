ad40d95eb72a6b24bd7b6fa608d71882a8a5dbb0
AdlerCastro <adlercastro01@gmail.com>
2024-07-11 01:08:00 -0300

    :construction: Arrumando o layout global

--- blog/src/app/layout.tsx
+++ blog/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/templates/Footer";
+import Footer from "@/app/components/organisms/Footer";
+import Header from "@/app/components/organisms/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header/>
+        {children}
+        <Footer/>
+      </body>
     </html>
   );
 }
--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,13 +1,9 @@
-import Header from "@/components/organisms/Header";
-import Footer from "@/components/templates/Footer";
 import Hero from "@/components/templates/Hero";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
-      <Footer />
     </>
   );
 }
