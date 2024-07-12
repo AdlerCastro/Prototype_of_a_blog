f44945158b95a8792373b7ae0ab8c14e464e5299
AdlerCastro <adlercastro01@gmail.com>
2024-07-12 00:32:10 -0300

    :construction: Verificando se o usuário está logado

--- blog/src/components/organisms/Header/index.tsx
+++ blog/src/components/organisms/Header/index.tsx
@@ -1,9 +1,17 @@
+'use client'
+
 import Image from 'next/image'
 import Anya from 'public/images/anyaPointing.png'
-import './styles.css'
 import Link from 'next/link'
+import { signOut, useSession } from 'next-auth/react'
+
+import './styles.css'
+import Button from '@/components/atoms/Button'
 
 const Header = () => {
+
+  const { data: session }: any = useSession();
+
   return (
     <header className='header'>
       <Image src={Anya} alt='Logo' className='image' />
@@ -14,14 +22,24 @@ const Header = () => {
           <Link href="/About">Sobre</Link>
           <Link href="/Contacts">Contato</Link>
         </nav>
-        <div className='interactions'>
-          <Link href='/Register'>
-            Cadastre-se
-          </Link>
-          <Link href='/Login'>
-            Login
-          </Link>
-        </div>
+        {!session ? (
+          <div className='interactions'>
+            <Link href='/Register'>
+              Cadastre-se
+            </Link>
+            <Link href='/Login'>
+              Login
+            </Link>
+          </div>
+
+        ) : (
+          <div className='interactions'>
+            {session.user?.email}
+            <Button onClick={() => {signOut()}} className="logout">
+              Sair
+            </Button>
+          </div>
+        )}
       </div>
     </header>
   )
