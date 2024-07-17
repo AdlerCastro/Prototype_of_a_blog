3ad006c6b9dcdbd1c04bd2d72a7c364e95a54991
AdlerCastro <adlercastro01@gmail.com>
2024-07-17 17:49:03 -0300

    :sparkles: feat: Página de post

--- blog/src/app/Posts/page.tsx
+++ blog/src/app/Posts/page.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
+import Link from 'next/link'
+import './styles.css'
 
 const Posts = async () => {
 
+    // Verificação de usuário logado
     const session = await getServerSession();
-    
+
     if (!session) {
-        redirect("/");
+        redirect("/Login");
     }
+
     return (
-        <div>Posts</div>
+        <main className='Posts'>
+            <div className='top-posts'>
+                <h1>Posts</h1>
+                <Link href='/Posts/CreatePost'>+Criar</Link>
+            </div>
+            <div className='view-posts'>
+            <h2>Os posts ficarão aqui</h2>
+            </div>
+        </main>
     )
 }
 
