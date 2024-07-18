fe61da6851ca8aa49e16bc3ef2d7b706acfdf5b2
AdlerCastro <adlercastro01@gmail.com>
2024-07-18 12:45:45 -0300

    :ok_hand: style: Removendo div desnecessária

--- blog/src/app/Posts/page.tsx
+++ blog/src/app/Posts/page.tsx
@@ -7,31 +7,29 @@ import Link from 'next/link'
 import './styles.css'
 
 export default function Posts() {
-    
-    const router = useRouter();
-    const { data: session, status: sessionStatus } = useSession();
 
-    // Verificação de usuário logado
-    useEffect(() => {
-        if (sessionStatus === 'unauthenticated') {
-          router.replace("/Login")
-        }
+  const router = useRouter();
+  const { data: session, status: sessionStatus } = useSession();
 
-      }, [sessionStatus, router])
+  // Verificação de usuário logado
+  useEffect(() => {
+    if (sessionStatus === 'unauthenticated') {
+      router.replace("/Login")
+    }
 
-      if (sessionStatus === "loading") {
-        return <h1>Calma...</h1>;
-      }
+  }, [sessionStatus, router])
 
-    return (
-        <main className='Posts'>
-            <div className='top-posts'>
-                <h1>Posts</h1>
-                <Link href='/Posts/CreatePost'>+Criar</Link>
-            </div>
-            <div className='view-posts'>
-            <h2>Os posts ficarão aqui</h2>
-            </div>
-        </main>
-    )
+  if (sessionStatus === "loading") {
+    return <h1>Calma...</h1>;
+  }
+
+  return (
+    <main className='Posts'>
+      <h1>Posts</h1>
+      <Link href='/Posts/CreatePost'>+Criar</Link>
+      <div className='view-posts'>
+        <h2>Os posts ficarão aqui</h2>
+      </div>
+    </main>
+  )
 }
