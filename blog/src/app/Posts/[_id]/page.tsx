0319578dc72a5a39a1a956beca8ca797e3a6bb24
AdlerCastro <adlercastro01@gmail.com>
2024-07-23 17:47:22 -0300

    :sparkles: feat: Visualizar post individuais

---
+++ blog/src/app/Posts/[_id]/page.tsx
@@ -0,0 +1,8 @@
+export default function PostDetails() {
+
+  return (
+    <>
+        <h1>ViewPost</h1>
+    </>
+  )
+}
--- blog/src/app/Posts/page.tsx
+++ blog/src/app/Posts/page.tsx
@@ -2,11 +2,22 @@
 
 import { getPosts } from '../actions/posts'
 import { useQuery } from 'react-query'
+import { useRouter } from 'next/navigation'
 
-import Link from 'next/link'
+import Button from '@/components/atoms/Button'
+import { Loading } from '@/components/templates/Loading'
 import './styles.css'
 
 export default function Posts() {
+  const router = useRouter();
+
+  const routerForward = () => {
+    router.push('/Posts/CreatePost')
+  }
+
+  const ViewPost = (_id: string) => {
+    router.push(`/Posts/${_id}`);
+  }
 
   const { data, isLoading } = useQuery({
     queryFn: async () => await getPosts(),
@@ -14,18 +25,19 @@ export default function Posts() {
   })
 
   if (isLoading) {
-    return <h1>Calma...</h1>;
+    return <Loading />
   }
 
   return (
     <main className='Posts'>
       <h1>Posts</h1>
-      <Link href='/Posts/CreatePost'>+Criar</Link>
+      <Button className="buttonRouter" onClick={() => routerForward()}>+Criar</Button>
       <div className='view-posts'>
         {data.map((post: any) => (
           <div key={post._id} className='card-post'>
             <h2>{post.title}</h2>
             <p>{post.description}</p>
+            <button onClick={() => ViewPost(post._id)}>Read more</button>
           </div>
         ))}
       </div>
