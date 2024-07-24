e51a59434d0319221413f2803b7f3f29800420a2
AdlerCastro <adlercastro01@gmail.com>
2024-07-23 22:48:30 -0300

    :sparkles: feat: Notificando o usuário que obteve sucesso

--- blog/src/app/Posts/CreatePost/page.tsx
+++ blog/src/app/Posts/CreatePost/page.tsx
@@ -5,6 +5,7 @@ import { useMutation } from 'react-query';
 
 import Button from '@/components/atoms/Button';
 import './styles.css'
+import { POST } from '@/app/api/createPost/route';
 
 export default function CreatePost() {
 
@@ -14,39 +15,49 @@ export default function CreatePost() {
     router.back()
   }
 
-  
+  // const {mutateAsync: createdPostFn } = useMutation({
+  //   mutationFn: POST,
+  // })
+
   async function createPost(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    
-    const mutation = useMutation({
-      mutationFn: async () => {
-        return fetch("/api/createPost")
-      },
-    })
+    const formData = new FormData(e.currentTarget);
+    const title = formData.get("title");
+    const description = formData.get("description");
 
-    mutation.mutate()
+    // createdPostFn(
+    //   title,
+    //   description
+    // )
 
-  //   try {
-  //     await fetch("/api/createPost", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json"
-  //       },
-  //       body: JSON.stringify({
-  //         title,
-  //         description
-  //       })
-  //     })
+    try {
+      const res = await fetch("/api/createPost", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          title,
+          description
+        })
+      })
 
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
+      if (res.status === 202) {
+        alert("Post criado com sucesso!")
+      }
+      if (res.status === 501) {
+        alert("Erro ao criar post, tente novamente")
+      }
+
+    } catch (error) {
+      alert(error)
+    }
   }
 
   return (
     <main className='create-post'>
       <h1>Criando Post</h1>
-      <Button className = "routerBack" onClick={() => routerBack()}>Voltar</Button>
+      <Button className="routerBack" onClick={() => routerBack()}>Voltar</Button>
       <div className='space-form'>
         <form onSubmit={createPost} className='form-create-post'>
           <h4>Título</h4>
