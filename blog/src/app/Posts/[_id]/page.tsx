a4478dc7e5d34ec5aaec9345b3bb0b1bab7d5994
AdlerCastro <adlercastro01@gmail.com>
2024-07-24 03:00:41 -0300

    :construction: Resgatar os dados do post

--- blog/src/app/Posts/[_id]/page.tsx
+++ blog/src/app/Posts/[_id]/page.tsx
@@ -2,22 +2,22 @@
 
 import { useQuery } from 'react-query'
 import { Loading } from '@/components/templates/Loading'
-import { useParams, useRouter } from 'next/navigation'
+import { useParams } from 'next/navigation'
 import { getPostById } from '@/app/actions/getPostById'
 
 import './styles.css'
-import Button from '@/components/atoms/Button'
+
+interface Post {
+  _id: string;
+  title: string;
+  description: string;
+}
 
 export default function PostDetails() {
-  const router = useRouter();
+  const { _id } = useParams() as { _id: string };
+  console.log(_id)
 
-  const routerBack = () => {
-    router.back()
-  }
-  
-  const { _id } = useParams();
-
-  const { data, isLoading, error } = useQuery({
+  const { data, isLoading, error } = useQuery<Post>({
     queryFn: async () => await getPostById(_id),
     queryKey: ["Posts", _id],
   })
@@ -33,7 +33,6 @@ export default function PostDetails() {
   return (
     <main className='view-details-posts'>
       <h1>View Post</h1>
-      <Button className="routerBack" onClick={() => routerBack()}>Voltar</Button>
       <div className='space-view'>
         <div className='card-post-details'>
           <h2>{data?.title}</h2>
