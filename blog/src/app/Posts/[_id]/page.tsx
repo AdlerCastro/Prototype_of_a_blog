f28a7319e526ed384a269d6ab187f1bc831f8803
AdlerCastro <adlercastro01@gmail.com>
2024-07-24 01:15:09 -0300

    :construction: Resgatando os dados individuais do post

--- blog/src/app/Posts/[_id]/page.tsx
+++ blog/src/app/Posts/[_id]/page.tsx
@@ -1,8 +1,45 @@
-export default function PostDetails() {
+'use client'
 
+import { useQuery } from 'react-query'
+import { Loading } from '@/components/templates/Loading'
+import { useParams, useRouter } from 'next/navigation'
+import { getPostById } from '@/app/actions/getPostById'
+
+import './styles.css'
+import Button from '@/components/atoms/Button'
+
+export default function PostDetails() {
+  const router = useRouter();
+
+  const routerBack = () => {
+    router.back()
+  }
+  
+  const { _id } = useParams();
+
+  const { data, isLoading, error } = useQuery({
+    queryFn: async () => await getPostById(_id),
+    queryKey: ["Posts", _id],
+  })
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  if (error) {
+    return <p>Erro ao carregar o post</p>
+  }
+  
   return (
-    <>
-        <h1>ViewPost</h1>
-    </>
+    <main className='view-details-posts'>
+      <h1>View Post</h1>
+      <Button className="routerBack" onClick={() => routerBack()}>Voltar</Button>
+      <div className='space-view'>
+        <div className='card-post-details'>
+          <h2>{data?.title}</h2>
+          <p>{data?.description}</p>
+        </div>
+      </div>
+    </main>
   )
 }
