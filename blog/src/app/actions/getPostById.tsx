3d9e8095019c5553e9f85091226786a63b9e4e44
AdlerCastro <adlercastro01@gmail.com>
2024-07-25 10:36:24 -0300

    :ok_hand: test: debug

--- blog/src/app/actions/getPostById.tsx
+++ blog/src/app/actions/getPostById.tsx
@@ -2,13 +2,13 @@
 
 export async function getPostById(_id: string) {
   try {
+    console.log('Resgatando o post com ID:', _id);
     const res = await fetch(`http://localhost:3000/api/getPostById/${_id}`);
     console.log(res)
-    console.log('Resgatando o post com ID:', _id);
 
     if (!res.ok) {
       console.log('Response status:', res.status);
-      throw new Error('Falha ao resgatar o post');
+      throw new Error(`Falha ao resgatar o post! status: ${res.status}`);
     }
 
     const post = await res.json()
