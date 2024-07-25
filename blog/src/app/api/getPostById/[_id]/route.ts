4fd5b49240c116daf3261936e55bd454b81912cc
AdlerCastro <adlercastro01@gmail.com>
2024-07-25 10:37:51 -0300

    :bug: fix: _id Params undefined

--- blog/src/app/api/getPostById/[_id]/route.ts
+++ blog/src/app/api/getPostById/[_id]/route.ts
@@ -2,19 +2,26 @@ import Posts from '@/utils/models/Posts';
 import Connect from '@/utils/database/db';
 import { NextResponse } from 'next/server';
 
-export const GET = async ({ params }: { params: { _id: string } }) => {
-  
-  const { _id } = params;
+interface PostURL {
+    _id: string;
+}
+
+export const GET = async (request: Request, context: {params: PostURL}) => {
+  // const _id = "66a05bb7373e5a728e1d68c7"; //teste para verificar funcionalidade
+  const _id = context.params._id;
   console.log(_id)
-  
+
   await Connect();
-  
+
   try {
+
     const post = await Posts.findById(_id);
+
     if (!post) {
       return new NextResponse('Post não encontrado', { status: 404 });
     }
-    return NextResponse.json(post);
+
+    return new NextResponse(JSON.stringify(post), { status: 203 });
 
   } catch (error) {
     console.log(error)
